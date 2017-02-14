0acf9605a4ca0e9086ca38700a1d77f048a7f6b6
Eda <eda.meadows@gmail.com>
2017-02-14 14:35:40 +0000

    DC-815 completed Play 2.5 migration

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -21,22 +21,22 @@ private object AppDependencies {
   def apply() = Seq(
 
     ws,
-    "uk.gov.hmrc"                 %% "frontend-bootstrap"    % "7.10.0",
+    "uk.gov.hmrc"                 %% "frontend-bootstrap"       % "7.11.0",
     "uk.gov.hmrc"                 %% "play-authorised-frontend" % "6.2.0",
-    "uk.gov.hmrc"                 %% "play-health"            % "2.0.0",
-    "uk.gov.hmrc"                 %% "play-config"            % "3.0.0",
-    "uk.gov.hmrc"                 %% "logback-json-logger"    % "3.1.0",
-    "uk.gov.hmrc"                 %% "play-partials"          % "5.2.0",
-    "uk.gov.hmrc"                 %% "govuk-template"         % "5.0.0",
-    "uk.gov.hmrc"                 %% "play-ui"                % "5.2.0",
-    "org.jsoup"                   %  "jsoup"                  % "1.9.2" % "test",
-    "com.github.tomakehurst"      %  "wiremock"               % "1.58"  % "test",
-    "uk.gov.hmrc"                 %% "hmrctest"               % "2.1.0" % "test",
-    "org.scalatest"               %% "scalatest"              % "2.2.6" % "test",
-    "org.pegdown"                 %  "pegdown"                % "1.6.0" % "test",
-    "org.mockito"                 %  "mockito-core"           % "1.9.0" % "test",
-    "org.scalatestplus.play"      %% "scalatestplus-play"     % "1.5.1" % "test",
-    "com.typesafe.play"           %% "play-test"              % PlayVersion.current % "test"
+    "uk.gov.hmrc"                 %% "play-health"              % "2.0.0",
+    "uk.gov.hmrc"                 %% "play-config"              % "3.1.0",
+    "uk.gov.hmrc"                 %% "logback-json-logger"      % "3.1.0",
+    "uk.gov.hmrc"                 %% "play-partials"            % "5.2.0",
+    "uk.gov.hmrc"                 %% "govuk-template"           % "5.0.0",
+    "uk.gov.hmrc"                 %% "play-ui"                  % "6.0.0",
+    "org.jsoup"                   %  "jsoup"                    % "1.9.2" % "test",
+    "com.github.tomakehurst"      %  "wiremock"                 % "1.58"  % "test",
+    "uk.gov.hmrc"                 %% "hmrctest"                 % "2.2.0" % "test",
+    "org.scalatest"               %% "scalatest"                % "2.2.6" % "test",
+    "org.pegdown"                 %  "pegdown"                  % "1.6.0" % "test",
+    "org.mockito"                 %  "mockito-core"             % "1.9.0" % "test",
+    "org.scalatestplus.play"      %% "scalatestplus-play"       % "1.5.1" % "test",
+    "com.typesafe.play"           %% "play-test"                % PlayVersion.current % "test"
   )
 }
 
