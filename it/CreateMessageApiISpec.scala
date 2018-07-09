b42c99b901d7e8e1fe2aa7750df8ed2a1ab71177
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-07-09 12:52:54 +0100

    it

---
+++ it/CreateMessageApiISpec.scala
@@ -0,0 +1,132 @@
+import java.util.UUID
+import org.joda.time.DateTime
+import org.jsoup.Jsoup
+import org.scalatest.DoNotDiscover
+
+import org.scalatest.concurrent.ScalaFutures
+import play.api.http.Status._
+import play.api.libs.json.{ JsObject, JsValue, Json }
+import play.api.libs.ws.{WS, WSResponse}
+import play.api.test.FakeRequest
+import uk.gov.hmrc.contactadvisors.connectors.MessageResponse
+import uk.gov.hmrc.domain._
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+import scala.concurrent.duration._
+import org.scalatest.BeforeAndAfterAll
+
+import uk.gov.hmrc.play.it.{ExternalServiceRunner, ExternalServicesServer, MongoMicroServiceEmbeddedServer, MongoTestConfiguration, ServiceSpec}
+import org.scalatest.concurrent.{Eventually, IntegrationPatience, ScalaFutures}
+
+import scala.concurrent.Future
+
+import uk.gov.hmrc.http.HttpResponse
+import org.joda.time.LocalDate
+
+class CreateMessageApiISpec extends UnitSpec
+    with ScalaFutures
+    with BeforeAndAfterAll
+    with Eventually {
+  self =>
+
+  lazy val server:CustomerAdvisorsIntegrationServer =
+    CustomerAdvisorsIntegrationServer.server
+
+  def resource(path: String): String = server.resource(path)
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    if (!CustomerAdvisorsIntegrationServer.serverControlledByItSuite) {
+      server.start()
+    }
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    if (!CustomerAdvisorsIntegrationServer.serverControlledByItSuite) {
+      server.stop()
+    }
+  }
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+  implicit lazy val app = play.api.Play.current
+
+  "POST /customer-advisors-frontend/submit" should {
+    "redirect to the success page when the form submission is successful" in {
+
+      val content = DateTime.now().toString
+      val fhddsRef = "XZFH00000100024"
+      val p = resource(s"secure-message/customer-advisors-frontend/submit?content=${content}21&subject=mysubject&recipientTaxidentifierName=sautr&recipientTaxidentifierValue=${fhddsRef}&recipientEmail=foo@domain.com&recipientNameLine1=rLine1&messageType=mType")
+      val response = WS.url(p ).post("")
+      response.status shouldBe OK
+      val body = response.futureValue.body
+      val document = Jsoup.parse(body)
+      withClue("result page title") {
+        document.title() shouldBe "Advice creation successful"
+      }
+      withClue("result page FHDDS Reference") {
+        document.select("ul li").get(0).text() shouldBe s"FHDDS Reference: ${fhddsRef}"
+      }
+      withClue("result page Message Id") {
+        document.select("ul li").get(1).text()  should startWith regex "Id: [0-9a-f]+"
+      }
+      withClue("result page External Ref") {
+        document.select("ul li").get(2).text()  should startWith regex "External Ref: [0-9a-f-]+"
+      }
+    }
+    "redirect to the unexpected page when the form submission is unsuccessful" in {
+
+      val content = DateTime.now().toString
+      val fhddsRef = "XZFH00000100024"
+      val wrongEmail = "foobar"
+      val p = resource(s"secure-message/customer-advisors-frontend/submit?content=${content}21&subject=mysubject&recipientTaxidentifierName=sautr&recipientTaxidentifierValue=${fhddsRef}&recipientEmail=${wrongEmail}&recipientNameLine1=rLine1&messageType=mType")
+      val response = WS.url(p ).post("")
+      response.status shouldBe OK
+      val body = response.futureValue.body
+      val document = Jsoup.parse(body)
+      withClue("result page title") {
+        document.title() shouldBe "Unexpected error"
+      }
+      withClue("result page title") {
+        document.select("p.alert__message").text() shouldBe(s"There is an unexpected problem. Secure inbox message has not been created for FHDDS Reference ${fhddsRef}")
+      }
+    }
+  }
+}
+
+class CustomerAdvisorsIntegrationServer(override val testName: String,
+  servicesFromJar: Seq[String],
+  servicesFromSource: Seq[String],
+  scenarioAdditionalConfig: Map[String, Any])
+    extends MongoMicroServiceEmbeddedServer {
+
+  override val externalServices = servicesFromJar.map(ExternalServiceRunner.
+    runFromJar(_)) ++ servicesFromSource.map(ExternalServiceRunner.runFromSource(_))
+
+  override protected val additionalConfig:Map[String, Any] = Map(
+    "Dev.microservice.services.customer-advisors-frontend.port" -> servicePort,
+    "ws.timeout.idle" -> "600000"
+  ) ++ scenarioAdditionalConfig
+
+  override protected def startTimeout = 240.seconds
+}
+
+object CustomerAdvisorsIntegrationServer {
+  val UserDetails = "user-details"
+  val Preferences = "preferences"
+  val EntityResolver = "entity-resolver"
+  val HmrcDeskPro = "hmrc-deskpro"
+  val SA = "sa"
+  val Datastream = "datastream"
+  val Message = "message"
+  val allExternalServices = Seq(
+    UserDetails,
+    Preferences,
+    EntityResolver,
+    SA,
+    Datastream,
+    Message
+  )
+  var serverControlledByItSuite = false
+
+  lazy val server = new CustomerAdvisorsIntegrationServer("ALL_CUSTOMER_ADVISORS_IT_TESTS", allExternalServices, Seq(), Map())
+}
