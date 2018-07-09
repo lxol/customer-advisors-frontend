5a1a5b5d3bb2278b65f05c38b9611a82b367ccfb
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-07-09 10:38:27 +0100

    WIP

--- test/uk/gov/hmrc/contactadvisors/dependencies/MessageStubV2.scala
+++ test/uk/gov/hmrc/contactadvisors/dependencies/MessageStubV2.scala
@@ -21,6 +21,7 @@ import org.skyscreamer.jsonassert.JSONCompareMode
 import play.api.http.Status
 import uk.gov.hmrc.contactadvisors.connectors.models.SecureMessageV2
 // import uk.gov.hmrc.utils.SecureMessageCreatorV2._
+import org.apache.commons.codec.binary.Base64
 import play.api.Logger
 
 trait MessageStubV2 {
@@ -54,38 +55,58 @@ trait MessageStubV2 {
     givenThat(
       post(urlEqualTo(messageEndpoint))
         .withRequestBody(
-          matching(".*")
-         //  equalToJson(
-         //    {
-         //    val foo = s"""
-         //       |{
-         //       |  "recipient": {
-         //       |    "taxIdentifier": {
-         //       |      "name": ${request.recipient.taxIdentifier.name}",
-         //       |      "value": ${request.recipient.taxIdentifier.value}"
-         //       |    }
-         //       |  "name":{
-         //       |      "line1": "${request.recipient.name.line1}"
-         //       |    },
-         //       |  "email":"${request.recipient.email}"
-         //       |  },
-         //       |  "externalRef": {
-         //       |    "id": "${request.externalRef.id}",
-         //       |    "source": "${request.externalRef.source}"
-         //       |  },
-         //       |  "messageType": "${request.messageType}",
-         //       |  "subject": "${request.subject}",
-         //       |  "content": "${request.content}",
-         //       |  "validFrom": "${request.validFrom}",
-         //       |  "alertQueue":"${request.alertQueue}"
-         //       |}
-         // """.stripMargin
-         //      Logger.info(s"****** request to compareTo: ${foo}")
-         //      foo
-         //      },
-         //    JSONCompareMode.LENIENT
-         //  )
+          // matching(".*")
+  // val uncleanContent = new String(Base64.encodeBase64("<p>This is the content of the secure message</p><script>alert('more hax')</script>".getBytes("UTF-8")))
+          equalToJson(
+            {
+            val foo = s"""
+               |{"recipient":
+               | {"taxIdentifier":
+               |  {"value":"XZFH00000100024",
+               |   "name":"HMRC-OBTDS-ORG"
+               |  },
+               |  "name":
+               |  {"line1":"Mr. John Smith"},
+               |  "email":"foo@bar.com"},
+               | "externalRef":
+               | {"id":"01012625-f783-4b63-a022-e01af0658687",
+               |  "source":"customer-advisor"
+               | },
+               | "messageType":"fhddsAlertMessage",
+               | "subject":"This is a response to your HMRC request<script>alert('hax')</script>",
+               | "content":"UEhBK1ZHaHBjeUJwY3lCMGFHVWdZMjl1ZEdWdWRDQnZaaUIwYUdVZ2MyVmpkWEpsSUcxbGMzTmhaMlU4TDNBK1BITmpjbWx3ZEQ1aGJHVnlkQ2duYlc5eVpTQm9ZWGduS1R3dmMyTnlhWEIwUGc9PQ==",
+               | "validFrom":"2018-07-09",
+               | "alertQueue":"PRIORITY"
+               |}
+         """.stripMargin
+              Logger.info(s"****** request to compareTo: ${foo}")
+              foo
+              },
+            JSONCompareMode.LENIENT
+          )
         )
         .willReturn(aResponse().withStatus(response._1).withBody(response._2)))
   }
 }
+
+               // |{
+               // |  "recipient": {
+               // |    "taxIdentifier": {
+               // |      "value": "${request.recipient.taxIdentifier.value}",
+               // |      "name": "${request.recipient.taxIdentifier.name}"
+               // |    },
+               // |    "name": {
+               // |      "line1": "${request.recipient.name.line1}"
+               // |    },
+               // |    "email":"${request.recipient.email}"
+               // |  },
+               // |  "externalRef": {
+               // |    "id": "${request.externalRef.id}",
+               // |    "source": "${request.externalRef.source}"
+               // |  },
+               // |  "messageType": "${request.messageType}",
+               // |  "subject": "${request.subject}",
+               // |  "content": "${new String(Base64.encodeBase64(request.content.getBytes("UTF-8")))}",
+               // |  "validFrom": "${request.validFrom}",
+               // |  "alertQueue":"${request.alertQueue}"
+               // |}
